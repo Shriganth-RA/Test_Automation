2a84f5778587bc17da68ec33f81cc78679680ca0
Shriganth RA <shriganth784@gmail.com>
2025-09-23 09:20:29 +0530

    Static web table added

--- cypress/e2e/test_automation.cy.js
+++ cypress/e2e/test_automation.cy.js
@@ -3,41 +3,117 @@ describe("test automation website", () => {
 
         // Navigate to the TestAutomation website
         cy.visit('https://testautomationpractice.blogspot.com/')
+
+// ----------------------------------------------------------------------------------------
         
         // Validate the Name feild
-        cy.get('#name').type("Sathish").should('have.value', 'Sathish');
+        cy.get('#name').type("Sathish")
+                            .should('have.value', 'Sathish');
+
+// ----------------------------------------------------------------------------------------
         
         // Validate the Email feild
-        cy.get('#email').type("sathish@gmail.com").should('have.value', 'sathish@gmail.com');
+        cy.get('#email').type("sathish@gmail.com")
+                            .should('have.value', 'sathish@gmail.com');
+
+// ----------------------------------------------------------------------------------------
         
         // Validate the Contact feild
-        cy.get('#phone').type("8941236719").should('have.value', '8941236719');
+        cy.get('#phone').type("8941236719")
+                            .should('have.value', '8941236719');
+
+// ----------------------------------------------------------------------------------------
         
         // Validate the Address feild
-        cy.get('#textarea').type("123, Gandhi road").should('have.value', '123, Gandhi road');
+        cy.get('#textarea').type("123, Gandhi road")
+                            .should('have.value', '123, Gandhi road');
+
+// ----------------------------------------------------------------------------------------
         
         // Validate the Gender button
-        cy.get('#male').click().should('be.checked').should('have.value', 'male');
+        cy.get('#male').click()
+                            .should('be.checked')
+                                .should('have.value', 'male');
+
+// ----------------------------------------------------------------------------------------
         
         // Validate the Days feild
         cy.get('#sunday').click().should('have.value', 'sunday');
         cy.get('#thursday').click().should('have.value', 'thursday');
-    
-        cy.get('select').get('#country').select('India').should('have.value', 'india');
-        
-        cy.get('select#colors').select('Blue').invoke('val').should('deep.equal', ['blue']);
-        
-        cy.get('select#animals').select('Cat').invoke('val').should('deep.equal', ['cat']);
-        
-        cy.get('input#datepicker').type("09/20/2025").should('have.value', '09/20/2025');
 
-        // cy.get('input#txtDate').type("10/20/2025").should('have.value', '10/20/2025');
+// ----------------------------------------------------------------------------------------
     
-        cy.get('input#start-date').type("2025-10-10").should('have.value', '2025-10-10');
+        cy.get('select').get('#country')
+                            .select('India')
+                                .should('have.value', 'india');
 
-        cy.get('input#end-date').type("2025-10-20").should('have.value', '2025-10-20');
+// ----------------------------------------------------------------------------------------
+        
+        cy.get('select#colors').select('Blue')
+                            .invoke('val')
+                                .should('deep.equal', ['blue']);
+
+// ----------------------------------------------------------------------------------------
+        
+        cy.get('select#animals').select('Cat')
+                            .invoke('val')
+                                .should('deep.equal', ['cat']);
+
+// ----------------------------------------------------------------------------------------
+        
+        let datePicker1 = "09/20/2025";
+        cy.get('input#datepicker').type(datePicker1)
+                                        .should('have.value', datePicker1);
+
+// ----------------------------------------------------------------------------------------
+
+        cy.press('Tab');
+        let datePicker2 = "22/09/2025";
+        cy.get('input#txtDate').invoke('removeAttr', 'readonly')
+                                    .type(datePicker2, {force: true})
+                                        .should('have.value', datePicker2);
+
+// ----------------------------------------------------------------------------------------
+    
+        cy.get('input#start-date').type("2025-10-10")
+                                        .should('have.value', '2025-10-10');
+
+        cy.get('input#end-date').type("2025-10-20")
+                                        .should('have.value', '2025-10-20');
 
         cy.get('button.submit-btn').click();
+
+// ----------------------------------------------------------------------------------------
+
+        // Upload a single file
+        const singleFile = 'Gowtham_file.txt';
+        cy.get('#singleFileInput').attachFile(singleFile);   // File to be upload
+        cy.get('#singleFileForm').contains('Upload Single File').click();   // Click the upload button
+        cy.get('#singleFileStatus').should('contain', 'Gowtham_file.txt');     // Verify the file was uploaded
+
+// ----------------------------------------------------------------------------------------
+
+        // Upload a multiple files
+        const multipleFile = [`cypress/fixtures/Arun_file.txt`, 'cypress/fixtures/Gowtham_file.txt', 'cypress/fixtures/Sathish_file.txt'];
+        cy.get('#multipleFilesInput').selectFile(multipleFile);     // Files to be upload
+        cy.get('#multipleFilesForm').contains('Upload Multiple Files').click();     // Click the upload button
+        
+        const checkMultipleFiles = ['Arun_file.txt', 'Gowtham_file.txt', 'Sathish_file.txt'];
+        checkMultipleFiles.forEach((file) => {
+            cy.get('#multipleFilesStatus').should('contain', file);     // Verify the files were uploaded
+        })
+
+// ----------------------------------------------------------------------------------------
+
+        // Static web table
+        const bookList = [
+            {bookName: 'Learn Selenium', author: 'Amit', subject: 'Selenium', price: '300'},
+            {bookName: 'Learn Java', author: 'Mukesh', subject: 'Java', price: '500'},
+        ]
+
+        cy.verifyTableCells(bookList);
+
+// ----------------------------------------------------------------------------------------
     
         // Pagination web table
         cy.get('#productTable input[type="checkbox"]').then(($checkboxes) => {
@@ -74,13 +150,26 @@ describe("test automation website", () => {
             }
         })
 
+// ----------------------------------------------------------------------------------------
+
         // Form
-        cy.get('#section1 #input1').type("Lorem ipsum").should('have.value', 'Lorem ipsum');
+        cy.get('#section1 #input1').type("Lorem ipsum")
+                                        .should('have.value', 'Lorem ipsum');
         cy.get('#btn1').trigger('mouseover').click();
-        cy.get('#section2 #input2').type("Lorem ipsum").should('have.value', 'Lorem ipsum');
+
+        cy.get('#section2 #input2').type("Lorem ipsum")
+                                        .should('have.value', 'Lorem ipsum');
         cy.get('#btn2').trigger('mouseover').click();
-        cy.get('#section3 #input3').type("Lorem ipsum").should('have.value', 'Lorem ipsum');
+
+        cy.get('#section3 #input3').type("Lorem ipsum")
+                                        .should('have.value', 'Lorem ipsum');
         cy.get('#btn3').trigger('mouseover').click();
-        
+
+// ----------------------------------------------------------------------------------------
+
+        // Footer
+        // cy.get('#PageList1 a').contains('Home').click();
+        // cy.get('#PageList1 a').contains('Hidden Elements & AJAX').click();
+        // cy.url().should('include', '/p/gui-elements-ajax-hidden.html');
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,7 +8,7 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
-//
+//cypress/support/commands.js
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
@@ -23,3 +23,41 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import { expect } from 'chai';
+import 'cypress-file-upload';
+
+// Static web table
+Cypress.Commands.add('verifyTableCells', (bookList) => {
+
+        bookList.forEach(book => {
+            cy.get('table[name="BookTable"] tbody tr').each(($row) => {
+                const bookName = $row.find('td').eq(0).text().trim();
+
+                if (bookName === book.bookName) {
+
+                    cy.wrap($row).find('td')
+                                .eq(1)
+                                .invoke('text')
+                                .then(text => text.trim())
+                                .should('eq', book.author)
+                                .then(text => cy.log(`Checking: ${bookName} | Author: ${text}`));
+
+                    cy.wrap($row).find('td')
+                                .eq(2)
+                                .invoke('text')
+                                .then(text => text.trim())
+                                .should('eq', book.subject)
+                                .then(text => cy.log(`Checking: ${bookName} | Subject: ${text}`));
+
+                    cy.wrap($row).find('td')
+                                .eq(3)
+                                .invoke('text')
+                                .then(text => text.trim())
+                                .should('eq', book.price)
+                                .then(text => cy.log(`Checking: ${bookName} | Price: ${text}`))
+                }
+            });
+        });
+    });
+
